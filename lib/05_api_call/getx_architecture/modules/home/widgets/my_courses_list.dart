398251a3ad595ca56e8a6edb07d00d69dda880e6
ajaykumarbcoder <ajaykumarbcoder@gmail.com>
2026-05-21 18:31:32 +0530

    feat: API Call App UI

---
+++ lib/05_api_call/getx_architecture/modules/home/widgets/my_courses_list.dart
@@ -0,0 +1,51 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class MyCoursesList extends StatelessWidget {
+  final String imageUrl;
+  final int enrollUsers;
+  final String title;
+  final String rating;
+  final String description;
+  final List<String> tags;
+  final int lectures;
+  final int chapters;
+
+  const MyCoursesList(
+      {super.key,
+      required this.imageUrl,
+      required this.title,
+      required this.description,
+      required this.chapters,
+      required this.enrollUsers,
+      required this.lectures,
+      required this.rating,
+      required this.tags});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(24),
+          ),
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                CachedNetworkImage(
+                  imageUrl: imageUrl,
+                  height: 200,
+                  width: double.infinity,
+                ),
+                Positioned(
+                    child: Container(color: Colors.white,
+                  child: Text('enrolled: $enrollUsers'),
+                ))
+              ],
+            ),
+          ],
+        ));
+  }
+}
